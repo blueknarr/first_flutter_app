9b00d4c7eeb809e4639631c06976b19383d678a2
blueknarr <knarrblue@gmail.com>
2023-06-12 23:52:01 +0900

    014. Scaffold를 사용하여 머티리얼 디자인의 뼈대와 AppBar 작성

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomnePageState extends State<MyHomePage> {
-  /// Container() : 기본적으로 아무것도 없기때문에 검은색 화면을 출력한다.
+  /// Scaffold() : 앱을 만드는 뼈대가 되는 기본 위젯
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      /// AppBar() 속성을 이용해 화면 상단에 Bar를 생성한다.
+      appBar: AppBar(
+        /// Text() 위젯을 사용해 글씨를 나타낸다.
+        title: Text('홈'),
+      ),
+
+      /// Center() 속성을 이용해 0을 중앙에 나타낸다.
+      body: Center(
+        child: Text(
+          '0',
+
+          /// TextStyle() 속성으로 폰트 색상과 크기를 설정한다.
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 70,
+          ),
+        ),
+      ),
+    );
   }
 }
